5a5910b58b49e5c92f169387b032135e7af443ee
Luan Rodrigues <luanlrs96@gmail.com>
2021-03-08 21:05:16 -0300

    Aprendendo a criar módulos

---
+++ primeiro-projeto/src/app/cursos/cursos.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Imports
+import { CursosComponent } from './cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+
+
+@NgModule({
+  declarations: [
+    CursosComponent,
+    CursoDetalheComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CursosComponent
+  ]
+})
+export class CursosModule { }
