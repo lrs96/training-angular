43e06df34a532f3ce0cacd45614d01cdab710dd9
Luan Rodrigues <luanlrs96@gmail.com>
2021-03-18 16:29:43 -0300

    pipes

---
+++ pipes/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+
+import ptBr from '@angular/common/locales/pt';
+
+import { AppComponent } from './app.component';
+import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
+import { CamelCasePipe } from './camel-case.pipe';
+import { SettingsService } from './settings.service';
+
+registerLocaleData(ptBr)
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ExemplosPipesComponent,
+    CamelCasePipe
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [
+    // {
+    //   provide: LOCALE_ID,
+    //   useValue: 'pt'
+    // }
+    SettingsService,
+    {
+      provide: LOCALE_ID,
+      deps: [SettingsService],
+      useFactory: (settingsService) => settingsService.getLocale()
+    }
+
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
